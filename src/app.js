c227d90c27aa8f0db4fcf8d6328820f7fc4d089e
Diego Guisasola <diego.guisasola@utp.edu.co>
2022-06-18 08:11:01 -0500

    Added port and views configuration and morgan

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
+const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 // Import routes
 
 // Settings
+app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Middlewares
-
+app.use(morgan('dev'));
 
 
 
 // Start the server
-app.listen(3000, () => {
-    console.log('Server on port 3000');
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
 });
