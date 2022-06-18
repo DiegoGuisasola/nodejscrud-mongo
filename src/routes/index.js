372c06077d3a1e426f9769ddca1a1b77f33b131b
Diego Guisasola <diego.guisasola@utp.edu.co>
2022-06-18 18:17:24 -0500

    Receiving new object from front

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,5 +7,9 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.post('/add', (req, res) => {
+    console.log(req.body);
+    res.send('received');
+});
 
 module.exports = router;
