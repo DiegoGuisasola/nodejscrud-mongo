5e08a1f2754eb28b1d325d191dbe14563b80ad58
Diego Guisasola <diego.guisasola@utp.edu.co>
2022-06-18 18:21:51 -0500

    Added async await

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+// Schema
+const Task = require('../models/task');
+
 // Routes
 router.get('/', (req, res) => {
     // res.send('Hello world');
     res.render('index');
 });
 
-router.post('/add', (req, res) => {
-    console.log(req.body);
+router.post('/add', async (req, res) => {
+    const task = new Task(req.body);
+    await task.save();
     res.send('received');
 });
 
