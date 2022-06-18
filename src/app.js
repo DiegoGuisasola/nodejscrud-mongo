8f5796d53ad04045e4308135397200ef0d342a0a
Diego Guisasola <diego.guisasola@utp.edu.co>
2022-06-18 14:05:50 -0500

    Added forms using express.urlencoded

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const indexRoutes = require('./routes/index');
 app.set('port', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
 
 // Middlewares
 app.use(morgan('dev'));
