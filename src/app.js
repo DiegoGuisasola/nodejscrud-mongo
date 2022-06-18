eb8d84d4f5e294f127821e586cf17ae8bd032a9f
Diego Guisasola <diego.guisasola@utp.edu.co>
2022-06-18 07:29:51 -0500

    Initial layout

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+// Import routes
+
+// Settings
+
+// Middlewares
+
+
+
+
+// Start the server
 app.listen(3000, () => {
     console.log('Server on port 3000');
 });
